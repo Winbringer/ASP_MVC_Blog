e618d26593a265daa874e5379c015d45b92b1849
Winbringer <voin1213@yandex.ru>
2016-06-12 00:10:28 +0700

    l1

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
+            db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Price = 180 });
+            db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Price = 150 });
+
+            base.Seed(db);
+        }
+    }
+
+    public class Book
+    {
+        // ID книги
+        public int Id { get; set; }
+        // название книги
+        public string Name { get; set; }
+        // автор книги
+        public string Author { get; set; }
+        // цена
+        public int Price { get; set; }
+    }
+
+    public class Purchase
+    {
+        // ID покупки
+        public int PurchaseId { get; set; }
+        // имя и фамилия покупателя
+        public string Person { get; set; }
+        // адрес покупателя
+        public string Address { get; set; }
+        // ID книги
+        public int BookId { get; set; }
+        // дата покупки
+        public DateTime Date { get; set; }
+    }
+}
