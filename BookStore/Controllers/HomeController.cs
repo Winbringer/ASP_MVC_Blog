91e5340332e23280edd8afef36d1f4c49c2e4479
Winbringer <voin1213@yandex.ru>
2016-08-15 20:16:31 +0300

    Добавил страницу для просмотра айпи адреса

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -12,6 +12,12 @@ namespace BookStore.Controllers
         // создаем контекст данных
         BookContext db = new BookContext();
 
+        protected override void Dispose(bool disposing)
+        {
+            db?.Dispose();
+            base.Dispose(disposing);
+        }
+
         public ActionResult Index()
         {
             // получаем из бд все объекты Book
@@ -38,6 +44,48 @@ namespace BookStore.Controllers
             // сохраняем в бд все изменения
             db.SaveChanges();
             return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+
+        public string Square()
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int h = Int32.Parse(Request.Params["h"]);
+            double s = a * h / 2;
+            return "<h2>Площадь треугольника с основанием " + a + " и высотой " + h + " равна " + s + "</h2>";
+        }
+
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Привет мир!</h2>");
+        }
+
+        public string Ip()
+        {
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
+        }
+    }
+    public class HtmlResult : ActionResult
+    {
+        private string htmlCode;
+        public HtmlResult(string html)
+        {
+            htmlCode = html;
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            string fullHtmlCode = "<!DOCTYPE html><html><head>";
+            fullHtmlCode += "<title>Главная страница</title>";
+            fullHtmlCode += "<meta charset=utf-8 />";
+            fullHtmlCode += "</head> <body>";
+            fullHtmlCode += htmlCode;
+            fullHtmlCode += "</body></html>";
+            context.HttpContext.Response.Write(fullHtmlCode);
         }
     }
 }
